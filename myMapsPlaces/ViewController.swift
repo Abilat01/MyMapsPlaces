ce800da6b98b42a028c21e8eda311e922907db79
Abilat01 <Abilat01@yandex.ru>
2021-05-23 13:37:57 +0300

    Initial Commit

---
+++ myMapsPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myMapsPlaces
+//
+//  Created by Danya on 23.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
